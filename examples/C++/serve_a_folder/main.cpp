660734349614fed0acd84973943ad824da5483a0
Patrick Wyatt <pat@onemoregame.com>
2023-12-03 19:50:04 -0800

    Add favicon

--- examples/C++/serve_a_folder/main.cpp
+++ examples/C++/serve_a_folder/main.cpp
@@ -7,6 +7,10 @@
 // Making this object global so show_second_window() can access it.
 webui::window my_second_window;
 
+const char s_icon[] = "<svg class=\"w-6 h-6 text-gray-800 dark:text-white\" aria-hidden=\"true\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 21 20\"><path stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"m11.479 1.712 2.367 4.8a.532.532 0 0 0 .4.292l5.294.769a.534.534 0 0 1 .3.91l-3.83 3.735a.534.534 0 0 0-.154.473l.9 5.272a.535.535 0 0 1-.775.563l-4.734-2.49a.536.536 0 0 0-.5 0l-4.73 2.487a.534.534 0 0 1-.775-.563l.9-5.272a.534.534 0 0 0-.154-.473L2.158 8.48a.534.534 0 0 1 .3-.911l5.294-.77a.532.532 0 0 0 .4-.292l2.367-4.8a.534.534 0 0 1 .96.004Z\"/></svg>";
+const char s_icontype[] = "image/svg+xml";
+
+
 // Example of a simple Class
 class MyClass {
 	public:
@@ -66,6 +70,7 @@ int main() {
 
 	// Create a new window
 	webui::window my_window;
+	my_window.set_icon(s_icon, s_icontype);
 
 	// Bind HTML element IDs with a C functions
 	my_window.bind("SwitchToSecondPage", switch_to_second_page_wrp);
